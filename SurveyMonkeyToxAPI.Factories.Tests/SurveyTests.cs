c345a422a8ce9a19ba76da92c4077b708488f06c
Gareth <gareth.marland@gmail.com>
2016-12-08 20:53:25 -0500

    adding survey test

---
+++ SurveyMonkeyToxAPI.Factories.Tests/SurveyTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SurveyMonkeyToxAPI.Models;
+using Newtonsoft.Json.Linq;
+
+namespace SurveyMonkeyToxAPI.Factories.Tests
+{
+    [TestClass]
+    public class SurveyTests
+    {
+        private string _surveyJSON = "{ \"title\": \"My Survey\", \"nickname\": \"\", \"category\": \"\", \"language\": \"en\", \"question_count\": 0, \"page_count\": 0, \"date_created\": \"2015-10-06T12:56:55+00:00\", \"date_modified\": \"2015-10-06T12:56:55+00:00\", \"id\": \"1234\", \"href\": \"http://api.surveymonkey.com/v3/surveys/1234\", \"buttons_text\": { \"done_button\": \"Done\", \"prev_button\": \"Prev\", \"exit_button\": \"Exit\", \"next_button\": \"Next\" }, \"custom_variables\": { \"name\": \"label\" }, \"preview\": \"https://www.surveymonkey.com/r/Preview/\", \"edit_url\": \"https://www.surveymonkey.com/create/\", \"collect_url\": \"https://www.surveymonkey.com/collect/list\", \"analyze_url\": \"https://www.surveymonkey.com/analyze/\", \"summary_url\": \"https://www.surveymonkey.com/summary/\", \"response_count\": 12 }";
+
+        [TestMethod]
+        public void GetSurveyStringPassing()
+        {
+            Survey survey = new Survey(_surveyJSON);
+
+            TestValidSurvey(survey);
+        }
+
+        [TestMethod]
+        public void GetSurveyJSONPassing()
+        {
+            JObject surveyJObject = JObject.Parse(_surveyJSON);
+
+            Survey survey = new Survey(surveyJObject);
+
+            TestValidSurvey(survey);
+        }
+        
+        private void TestValidSurvey(Survey survey)
+        {
+            Assert.AreEqual("1234", survey.Id);
+            Assert.AreEqual("My Survey", survey.Title);
+            Assert.AreEqual("en", survey.Language);
+            Assert.AreEqual(DateTime.Parse("2015-10-06T12:56:55+00:00"), survey.Created);
+            Assert.AreEqual(DateTime.Parse("2015-10-06T12:56:55+00:00"), survey.Modified);
+        }
+    }
+}
--- SurveyMonkeyToxAPI/Models/Survey.cs
+++ SurveyMonkeyToxAPI/Models/Survey.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace SurveyMonkeyToxAPI.Models
 {
     public class Survey
     {
-        public Survey(string id)
+        public Survey(string surveyJSON)
         {
-            Id = id;
+            JObject surveyJObject = JObject.Parse(surveyJSON);
+
+            PopulateSurvey(surveyJObject);
+        }
+
+        public Survey(JObject surveyJObject)
+        {
+            PopulateSurvey(surveyJObject);
+        }
+
+        private void PopulateSurvey(JObject surveyJObject)
+        {
+            if (surveyJObject["id"] != null) Id = (string)surveyJObject["id"];
+            else throw new Exception("A survey id could not be found");
+
+            if (surveyJObject["title"] != null) Title = (string)surveyJObject["title"];
+            else throw new Exception("A survey title could not be found");
+
+            if (surveyJObject["language"] != null) Language = (string)surveyJObject["language"];
+            else throw new Exception("A survey language could not be found");
+
+            if (surveyJObject["date_created"] != null) Created = (DateTime)surveyJObject["date_created"];
+            else throw new Exception("A survey date_created could not be found");
+
+            if (surveyJObject["date_modified"] != null) Modified = (DateTime)surveyJObject["date_modified"];
+            else throw new Exception("A survey date_modified could not be found");
+
+            Pages = new List<Page>();
         }
 
         public string Id { get; set; }
